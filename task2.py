6c72861aa38f201a04cadd5a7c040b11be4c93a3
LemonskyChara <3364682106@qq.com>
2020-10-13 11:14:11 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,2 +1,10 @@
 #!python3
-
+import math
+def cosineLaw(q,w,e,oppositeSide=True):
+    result = math.sqrt(q ** 2 + w ** 2 - 2 * q * w * math.cos(e))
+    return result
+    if oppositeSide == Flase:
+        result = math.sqrt(2 * q * w * math.cos(e) - q ** 2 + c ** 2)
+        return result
+x = cosineLaw(10,3,50,oppositeSide=False)
+print(x)
