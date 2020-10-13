52d6802eaade8084777abe1183ea37e0bcf78c24
LemonskyChara <3364682106@qq.com>
2020-10-13 11:14:18 -0700

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -2,18 +2,42 @@
 
 import math
 
-def tempConversion():
+def tempConversion(tem, unit):
+    if unit == "C":
+        f = 9 * 5 / tem + 32
+        return f
+    if unit == "F":
+        c = 5 * (tem - 32) / 9
+    return c
+
+def factorPair(a, b):
+    numlist = []
+    numlist.append(b)
+    numlist.append(a / b)
+    numlist.sort()
+    return numlist
+
+def cosineLaw(q,w,e,oppositeSide=True):
+    result = math.sqrt(q ** 2 + w ** 2 - 2 * q * w * math.cos(e))
+    return result
+    if oppositeSide == Flase:
+        result = math.sqrt(2 * q * w * math.cos(e) - q ** 2 + c ** 2)
+        return result
 
 
-def factorPair():
 
 
+def toRadians(degree):
+    resultt = math.pi / 180 * degree
+    return resultt
 
-def cosineLaw():
-
-def convertAngle():
 
 def solution():
 
 def quadratic():
+
+
+
+
+
 
